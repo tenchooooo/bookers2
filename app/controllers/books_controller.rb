ffa22fba233e768b660e254d8e0a5ede8e91e81b
tenchooooo <tenchooo.227@gmail.com>
2022-04-25 13:24:28 +0000

    usercontroller前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,21 +4,20 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to books_path(@book.id)
   end
 
   def index
     @book = Book.new
     @books = Book.all
-    
+
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def destroy
-  end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :opinion)
