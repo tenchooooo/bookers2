d75e488a8ffc46acfe4a39ba3de578319c541976
tenchooooo <tenchooo.227@gmail.com>
2022-04-27 13:07:50 +0000

    20章やればいいと思う

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
+  
+  def index
+    @users = User.all
+  end
 
   private
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :books
-  resources :users, only: [:show,:edit,:update]
+  resources :users, only: [:show,:edit,:update,:index]
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
 end
