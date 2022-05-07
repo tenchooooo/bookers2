8d96c282352b0287b0d5d0bd18af4138b91625a1
tenchooooo <tenchooo.227@gmail.com>
2022-05-07 15:20:57 +0000

    あと22

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,10 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @user = @book.user
+    if @user != current_user
+      redirect_to books_path
+    end
   end
 
   def update
